aaa9af99ac0840ef74782df0634e380bfc3c65e5
maxcastellano <maxime.castellano@etu.univ-cotedazur.fr>
2019-02-11 15:52:50 +0100

    Initialisation du projet

---
+++ src/sample/Main.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+
+        FXMLLoader loader = new FXMLLoader();
+        //Create a controller
+        Controller controller =new Controller();
+        //Attach controller
+        loader.setController(controller);
+        //Attach XML File
+        Parent root = loader.load(getClass().getResourceAsStream(View.XML_FILE));
+        //Attach css
+        //Initialize controller
+        controller.init();
+        //Create the view
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle(View.LABEL);
+        //Show the view
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/View.java
@@ -0,0 +1,6 @@
+package sample;
+
+public class View {
+    public static final String XML_FILE = "resources/polytech_home.fxml";
+    public static final String LABEL = "Accueil";
+}
