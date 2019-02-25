d2142185fc81c33545b1d9d1724705c993930f3c
Luca UGGERI <luca.uggeri@etu.unice.fr>
2019-02-25 16:31:00 +0100

    ajout choicebox

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import model.Model;
 
@@ -9,8 +10,19 @@ public class Controller {
     @FXML
     private ListView<String> listeDepense;
 
+    @FXML
+    private ChoiceBox<String> choicebox;
+
+
     public void init(){
         Model model = new Model();
         listeDepense.setItems(model.getListDepense());
+
+        choicebox.getItems().addAll("Annuel","Mensuel" ,"Hebdomadaire");
+        choicebox.getSelectionModel().select(1);
+
+
     }
+
+
 }
--- src/model/Model.java
+++ src/model/Model.java
@@ -12,9 +12,9 @@ public class Model {
 
     public Model(){
         listDepense = new ArrayList();
-        listDepense.add("2 Jan  7.50€");    //Tous les prix ont une tab
-        listDepense.add("4 Jan  12.50€");
-        listDepense.add("12 Jan  20.00€");
+        listDepense.add("2 Jan              7.50€");    //Tous les prix ont 4 tab
+        listDepense.add("4 Jan              12.50€");
+        listDepense.add("12 Jan             20.00€");
     }
 
     public ObservableList<String> getListDepense(){
