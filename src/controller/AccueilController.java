0b42183b5a43e13246033f60576cb23ba28bc638
PaulmarieDjekinnou <paul-marie.djekinnou@etu.unice.fr>
2019-03-04 15:47:57 +0100

    Transition accueil -> liste des courses, profil et historique d'achat

--- src/controller/AccueilController.java
+++ src/controller/AccueilController.java
@@ -1,10 +1,19 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 import model.AccueilModel;
+
+import javafx.event.*;
+import java.io.IOException;
+
+import static view.View.*;
 
 
 public class AccueilController {
@@ -14,7 +23,17 @@ public class AccueilController {
     @FXML
     private ChoiceBox<String> choicebox;
 
+    @FXML
+    private Button listedecoursesbouton;
 
+    @FXML
+    private Button articles;
+
+    @FXML
+    private Button historiquebouton;
+
+    @FXML
+    private Button profilbouton;
 
 
     public void init(){
@@ -23,8 +42,66 @@ public class AccueilController {
         choicebox.getItems().addAll("Annuel","Mensuel" ,"Hebdomadaire");
         choicebox.getSelectionModel().select(1);
 
-
+        listedecoursesbouton.setOnAction(event -> gotolistescourses(event));
+        profilbouton.setOnAction(event -> gotoprofil(event));
+        historiquebouton.setOnAction(event -> gothistorique(event));
     }
 
+    private void gotolistescourses(ActionEvent event) {
 
+        FXMLLoader loader = new FXMLLoader();
+
+        Parent listedepenseparent;
+        try {
+            listedepenseparent = loader.load(getClass().getResourceAsStream(LISTE_COURSES));
+            Scene listedepenseScene = new Scene(listedepenseparent);
+
+            Stage window = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+            window.setScene(listedepenseScene);
+            window.setTitle("Liste des dépenses");
+
+            window.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void gotoprofil(ActionEvent event){
+
+        FXMLLoader loader = new FXMLLoader();
+
+        Parent profilparent;
+        try {
+            profilparent = loader.load(getClass().getResourceAsStream(PROFIL));
+            Scene profilscene = new Scene(profilparent);
+
+            Stage window = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+            window.setScene(profilscene);
+            window.setTitle("Arcticles");
+
+            window.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void gothistorique(ActionEvent event){
+
+        FXMLLoader loader = new FXMLLoader();
+
+        Parent historiqueparent;
+        try {
+            historiqueparent = loader.load(getClass().getResourceAsStream(HISTORIQUE_DACHATS));
+            Scene historiquescene = new Scene(historiqueparent);
+
+            Stage window = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+            window.setScene(historiquescene);
+            window.setTitle("Historique d'achats");
+
+            window.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
